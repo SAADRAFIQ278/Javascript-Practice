dc27a88f8c897a5937e0bff0060004ae7b3ddfe1
SAADRAFIQ278 <mrk544939@gmail.com>
2026-07-31 13:31:13 -0700

    any msg allowed

---
+++ script.js
@@ -0,0 +1,104 @@
+// let firstName = "Saad";
+// console.log(firstName);
+
+// let age = 20;
+// console.log(age);
+
+// let city = "Peshawer";
+// let country = "Pakistan";
+// console.log(city,country);
+
+// let isStudent = true;
+// console.log(isStudent);
+
+// let set;
+// console.log(set);
+
+// let a = 10;
+// let b = 12;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// let marks = 85;
+// console.log(marks > 50);
+
+// let num = 15;
+// if(num%2==0){
+//     console.log("number is even");
+// }else{
+//     console.log("number is odd");
+    
+// }
+
+// let a = 20 ;
+// let b = 30 ;
+// if (a>b){
+//     console.log("A is greater than B");
+    
+// }else {
+//     console.log("B is greater than A");
+// }
+
+// let temperature = 40;
+// if(temperature>35){
+//     console.log("Weather is Hot")
+// }else{
+//     console.log("Weatheer is Normal");
+// }
+
+// let num = 0;
+// if (num > 0){
+//     console.log("Number is Positive");
+    
+// }else if(num < 0){
+//     console.log("Number is Negative");
+    
+// }else{
+//     console.log("Zero")
+// }
+
+// let password = 12345;
+// let correct = 12345;
+// if(password == correct){
+//     console.log("Login Successful");    
+// }else {
+//     console.log("Wrong Password");
+    
+// }
+
+// let marks = 80;
+// if (marks >= 90 ){
+//     console.log("You got grade A");
+// }else if(marks>=80){
+//     console.log("You got grade B");
+    
+// }else if(marks>=70){
+//     console.log("You got grade C");
+    
+// }else if(marks>=60){
+//     console.log("You got grade D");
+    
+// }else{
+//     console.log("You are fail");    
+// }    
+
+// let num = 45;
+// if (num % 3 == 0 && num % 5 == 0){
+//     console.log("It can be divided by both number");
+    
+// }else{
+//     console.log("It cant be divided by 3 & 5");
+    
+// }
+
+    
+
+
+
+
+
+
+
