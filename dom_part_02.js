194b3e3fd73c1c6af82c0bcd96776e13fd856581
SAADRAFIQ278 <mrk544939@gmail.com>
2026-09-22 19:50:56 -0700

    Commit by Saad

---
+++ dom_part_02.js
@@ -0,0 +1,40 @@
+// let nameInput = document.getElementById("nameInput")
+// let btn = document.getElementById("btn")
+// let message = document.getElementById("message")
+// btn.addEventListener("click",()=>{
+//     message.innerText = "Hello " + nameInput.value
+// })
+
+// let email = document.getElementById("email")
+// let btn = document.getElementById("btn")
+// let message = document.getElementById("message")
+// btn.addEventListener("click",()=>{
+//     if(email.value == ""){
+//         message.innerText = "Please Enter your email!"
+//     }
+// })
+
+
+
+// let email = document.getElementById("email")
+// let btn = document.getElementById("btn")
+// let message = document.getElementById("message")
+// btn.addEventListener("click",()=>{
+//     if(email.value === ""){
+//         message.innerText = "Please enter your email!"
+//     }else{
+//         message.innerText = "Registration Successful!"
+//     }
+// })
+
+
+// let name = document.getElementById("name")
+// let btn = document.getElementById("btn")
+// let message = document.getElementById("message")
+// btn.addEventListener("click", () => {
+//     if (name.value.trim() === "") {
+//         message.innerText = "Name is Required!"
+//     } else {
+//         message.innerText = "Hello " + name.value.trim()
+//     }
+// })
